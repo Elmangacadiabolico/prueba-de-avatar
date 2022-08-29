3838474f3545aa2a09bf62a1f20d3d842b2c3e52
Elmangacadiabolico <111751667+Elmangacadiabolico@users.noreply.github.com>
2022-08-29 09:19:03 -0300

    trabajo hecho
    xd

---
+++ avatare/script.js
@@ -0,0 +1,14 @@
+let ar = ["Ilon Mosk", "d10", "jugador hector", "car10s" ,"it0m" ]
+		
+document.getElementById("nombre").innerHTML = ar[Math.floor(Math.random() * 5)];
+
+let are = ["մենք մարeyehdgf չենdddddք", "ստիպեք dddfsafagtձեզ", "մենք չեgasfdsնք կծում... բայց եթե sfdggsdfgսպանենք", "me encanta la tierra por river" ,"me encanta el queso rallado molido por eso me gusta la tierra"]
+
+document.getElementById("card-text").innerHTML = are[Math.floor(Math.random() * 5)];
+
+let ad =["river es amor river es vida" ,"boca es odio boca es muerte","Hector aprobanos" ]
+
+document.getElementById("card-text2").innerHTML = ad[Math.floor(Math.random() * 3)];
+
+//integrantes francisco fernandez ,franco daniel racca ,leandro rios bas
+
